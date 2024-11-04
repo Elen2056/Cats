8c59e8daae386e755dc66b99599636306884c6b0
Elen2056 <eltacombis@gmail.com>
2024-11-04 15:26:47 +0300

    Добавлен combobox для выбора тегов, поле Entry удалено

--- Cataas.py
+++ Cataas.py
@@ -2,6 +2,12 @@ from tkinter import *
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+from tkinter import ttk
+
+# Список доступных тегов
+ALLOWED_TAGS = [
+    'sleep', 'jump', 'smile', 'fight', 'black', 'white', 'red', 'siamese', 'bengal'
+]
 
 
 def load_image(url):
@@ -18,13 +24,13 @@ def load_image(url):
 
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_with_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
     img = load_image(url_with_tag)
 
     if img:
         new_window = Toplevel()
-        new_window.title("Cat Image")
+     #   new_window.title("Картинка с котиком")
         new_window.geometry("600x480")
         label = Label(new_window, image=img)
         label.image = img
@@ -37,20 +43,40 @@ def exit_app():
 window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
-
-
 # file_menu.add_separator()
 # file_menu.add_command(label="Выход", command=exit_app)
-
 # Поле ввода для тегов
-tag_entry = Entry()
-tag_entry.pack()
+#tag_entry = Entry()
+#tag_entry.pack()
 
 # Кнопка для загрузки изображения с тегом
+# load_button = Button(text="Загрузить по тегу", command=open_new_window)
+# load_button.pack()
+
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+# Добавляем пункты меню
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit_app)
+
+url = 'https://cataas.com/cat'
+
+# Метка "Выбери тег"
+tag_label = Label(text="Выбери тег")
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=ALLOWED_TAGS)
+tag_combobox.pack()
+
 load_button = Button(text="Загрузить по тегу", command=open_new_window)
 load_button.pack()
 
 window.mainloop()
+
 
 
 
