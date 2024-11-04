7cace9a2f5bd0f143a19815c5cf0c21fa962b899
Elen2056 <eltacombis@gmail.com>
2024-11-04 10:08:17 +0300

    Добавлена загрузка по тегу

--- Cataas.py
+++ Cataas.py
@@ -2,25 +2,15 @@ from tkinter import *
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
-from _tkinter import *
+
 
 def load_image(url):
     try:
-        # Отправляем GET-запрос с использованием requests.get()
         response = requests.get(url)
-
-        # Проверяем успешность запроса (код ответа 200)
         response.raise_for_status()
-
-        # Читаем байты из ответа в объект BytesIO
         image_data = BytesIO(response.content)
-
-        # Открываем изображение с помощью PIL
         img = Image.open(image_data)
-
         img.thumbnail((600, 480), Image.Resampling.LANCZOS)
-
-
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Ошибка при загрузке изображения: {e}")
@@ -28,68 +18,41 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
-    if img:
-        # Создаем новое вторичное окно
-        new_window = Toplevel()
-        new_window.title("Картинка с котиком")
-        new_window.geometry("600x480")
+    tag = tag_entry.get()
+    url_with_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
+    img = load_image(url_with_tag)
 
-        # Добавляем изображение в новое окно
+    if img:
+        new_window = Toplevel()
+        new_window.title("Cat Image")
+        new_window.geometry("600x480")
         label = Label(new_window, image=img)
-        label.image = img  # Сохраняем ссылку на изображение
+        label.image = img
         label.pack()
 
-# def set_image():
-    # Вызываем функцию для загрузки изображения
-#    img = load_image(url)
 
-#    if img:
-        # Устанавливаем изображение в метку
-#        label.config(image=img)
-#        label.image = img   #Сохраняем ссылку на изображение
-
-def exit():
+def exit_app():
     window.destroy()
-
 
 window = Tk()
 window.title("Cats!")
-window.geometry("600x480")
-
-# Создаем метку без изображения
-label = Label()
-label.pack()
-
-# Создаем меню
-menu_bar = Menu(window)
-window.config(menu=menu_bar)
-
-# Добавляем пункты меню
-file_menu = Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Загрузить фото", command=open_new_window)
-file_menu.add_separator()
-file_menu.add_command(label="Выход", command=exit)
+window.geometry("600x520")
 
 
-# Добавляем кнопку для обновления изображения
-# update_button = Button(text="Обновить", command=set_image)
-# update_button.pack()
+# file_menu.add_separator()
+# file_menu.add_command(label="Выход", command=exit_app)
 
+# Поле ввода для тегов
+tag_entry = Entry()
+tag_entry.pack()
 
-url = 'https://cataas.com/cat'
-img = load_image(url)
-
-if img:
-    # Устанавливаем изображение в метку
-    label.config(image=img)
-    # Необходимо сохранить ссылку на изображение, чтобы избежать сборки мусора
-    label.image = img
-
-# Вызываем функцию для установки изображения в метку
-# set_image()
+# Кнопка для загрузки изображения с тегом
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 window.mainloop()
+
+
+
 
 
